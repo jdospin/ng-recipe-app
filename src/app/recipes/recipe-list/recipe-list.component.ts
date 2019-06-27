15db3451f67990acc88f103a29463a4c7f2ebfc8
Juan David <jdospin@hotmail.com>
2019-06-27 19:12:49 -0400

    Added recipe model. Added recipe model to the recipe-list component. Added ingredient model.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe',
+      'This is a recipe test',
+      'https://live.staticflickr.com/5496/31479301445_cb53c0f4e9_b.jpg' ),
+    new Recipe('Sea Food',
+      'This is a healthy recipe made of seafood',
+      'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
+    new Recipe('Pickled Garlic',
+      'A delicious pickle recipe',
+      'https://live.staticflickr.com/5836/30219331242_56aa4dfca9_b.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  ingredients = [];
   constructor() { }
 
   ngOnInit() {
